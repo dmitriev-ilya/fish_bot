fd19e5b33a98d5448a0b41874653a5eb9fd50722
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-07-17 21:24:55 +0500

    initial commit

---
+++ elasticpath_api_tools.py
@@ -0,0 +1,114 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+def get_access_token(client_id, client_secret):
+    api_access_url = 'https://useast.api.elasticpath.com/oauth/access_token'
+    api_access_data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(api_access_url, data=api_access_data)
+    response.raise_for_status()
+
+    access_token = response.json()['access_token']
+    return access_token
+
+
+def get_implicit_access_token(client_id):
+    api_access_url = 'https://useast.api.elasticpath.com/oauth/access_token'
+    api_access_data = {
+        'client_id': client_id,
+        'grant_type': 'implicit'
+    }
+    response = requests.post(api_access_url, data=api_access_data)
+    response.raise_for_status()
+
+    access_token = response.json()['access_token']
+    return access_token
+
+
+def get_products(access_token):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    product_api_base_url = 'https://useast.api.elasticpath.com/pcm/products'
+    params = {
+        'productId': "", 
+        'include': 'component_products'
+    }
+
+    responce = requests.get(product_api_base_url, params=params, headers=headers)
+    return responce.json()
+
+
+def create_cart(access_token, cart_id):
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    cart_api_url = 'https://useast.api.elasticpath.com/v2/carts'
+    cart_data = {
+        'data': {
+            'name': f'{cart_id}',
+            'id': cart_id
+        }
+    }
+
+    create_cart_responce = requests.post(cart_api_url, json=cart_data, headers=headers)
+    create_cart_responce.raise_for_status()
+    return create_cart_responce.json()
+
+
+def get_cart(access_token, cart_id):
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    cart_url = f'https://useast.api.elasticpath.com/v2/carts/{cart_id}'
+
+    geting_cart_response = requests.get(cart_url, headers=headers)
+    geting_cart_response.raise_for_status()
+    return geting_cart_response.json()
+
+
+def get_cart_items(access_token, cart_id):
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    cart_items_url = f'https://useast.api.elasticpath.com/v2/carts/{cart_id}/items'
+
+    response = requests.get(cart_items_url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def add_cart_item(access_token, cart_id, product_id, quantity):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+
+    item_data = {
+        'data': {
+            'id': product_id,
+            'type': 'cart_item',
+            'quantity': quantity
+        }
+    }
+    cart_items_url = f'https://useast.api.elasticpath.com/v2/carts/{cart_id}/items'
+
+    adding_cart_item_response = requests.post(cart_items_url, headers=headers, json=item_data)
+    adding_cart_item_response.raise_for_status()
+    return adding_cart_item_response.json()
+
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    access_token = get_access_token(os.getenv('CLIENT_ID'), os.getenv('CLIENT_SECRET'))
+
+    losos_id = 'efe48323-9521-408a-9bfc-b673dc8a2725'
+
+    print(get_cart_items(access_token, "dron"))
---
+++ tg_bot.py
@@ -0,0 +1,81 @@
+import os
+import logging
+import redis
+
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Filters, Updater
+from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler, CallbackContext
+
+_database = None
+
+
+def start(update: Update, context: CallbackContext ):
+    update.message.reply_text(text='Привет!')
+    return "ECHO"
+
+
+def echo(update: Update, context: CallbackContext):
+    users_reply = update.message.text
+    update.message.reply_text(users_reply)
+    return "ECHO"
+
+
+def handle_users_reply(update: Update, context: CallbackContext):
+    """
+    Функция, которая запускается при любом сообщении от пользователя и решает как его обработать.
+    Эта функция запускается в ответ на эти действия пользователя:
+        * Нажатие на inline-кнопку в боте
+        * Отправка сообщения боту
+        * Отправка команды боту
+    Она получает стейт пользователя из базы данных и запускает соответствующую функцию-обработчик (хэндлер).
+    Функция-обработчик возвращает следующее состояние, которое записывается в базу данных.
+    Если пользователь только начал пользоваться ботом, Telegram форсит его написать "/start",
+    поэтому по этой фразе выставляется стартовое состояние.
+    Если пользователь захочет начать общение с ботом заново, он также может воспользоваться этой командой.
+    """
+    db = get_database_connection()
+    if update.message.text:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id).decode("utf-8")
+    
+    states_functions = {
+        'START': start,
+        'ECHO': echo
+    }
+    state_handler = states_functions[user_state]
+
+    try:
+        next_state = state_handler(update, context)
+        db.set(chat_id, next_state)
+    except Exception as err:
+        print(err)
+
+def get_database_connection():
+    global _database
+    if _database is None:
+        database_password = os.getenv("DATABASE_PASSWORD")
+        database_host = os.getenv("DATABASE_HOST")
+        database_port = os.getenv("DATABASE_PORT")
+        _database = redis.Redis(host=database_host, port=database_port, password=database_password)
+    return _database
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    token = os.getenv("TELEGRAM_TOKEN")
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+    updater.start_polling()
