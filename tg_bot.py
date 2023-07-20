5618ff56d7b96dfa33960a447cbb6c79ee1f7c5e
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-07-20 20:41:52 +0500

    photo added to meassage

--- tg_bot.py
+++ tg_bot.py
@@ -1,41 +1,76 @@
 import os
 import logging
 import redis
+import textwrap
 
 from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Filters, Updater
 from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler, CallbackContext
 
-from elasticpath_api_tools import get_access_token, get_products
+from elasticpath_api_tools import get_access_token, get_products, get_product, get_file_href
 
 _database = None
 
 
-def start(update: Update, context: CallbackContext, milton_access_token):
+def send_fish_menu(milton_access_token):
     products = get_products(milton_access_token)['data']
     keyboard = [[]]
     for product in products:
         inline_item = InlineKeyboardButton(product['attributes']['name'], callback_data=product['id'])
         keyboard[0].append(inline_item)
     reply_markup = InlineKeyboardMarkup(keyboard)
-
-    update.message.reply_text(text='Привет! Ознакомся с нашим ассортиментом!', reply_markup=reply_markup)
-    return "ECHO"
+    return reply_markup
 
 
-def button(update: Update, context: CallbackContext, milton_access_token):
-    query = update.callback_query
-    update.message.reply_text(str(query.data))
+def start(update: Update, context: CallbackContext, milton_access_token):
+    reply_markup = send_fish_menu(milton_access_token)
+    update.message.reply_text(
+        text='Привет! Ознакомся с нашим ассортиментом!',
+        reply_markup=reply_markup
+    )
+    return 'HANDLE_MENU'
 
 
-def echo(update: Update, context: CallbackContext, milton_access_token):
-    if update.message:
-        user_reply = update.message.text
-    elif update.callback_query:
-        user_reply = update.callback_query.data
-    context.bot.send_message(chat_id=update.effective_chat.id, text=user_reply)
-    return "ECHO"
+def get_product_description(update: Update, context: CallbackContext, milton_access_token):
+    if update.callback_query.data == 'back':
+        reply_markup = send_fish_menu(milton_access_token)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='Наш ассортимент:',
+            reply_markup=reply_markup
+        )
+        context.bot.delete_message(
+            chat_id=update.effective_chat.id,
+            message_id=update.callback_query.message.message_id,
+        )
+        return 'HANDLE_MENU'
+    product_id = update.callback_query.data
+    product = get_product(milton_access_token, product_id)['data']
+    main_image_id = product['relationships']['main_image']['data']['id']
+    file_href = get_file_href(milton_access_token, main_image_id)
+    fish_description = f"""
+        {product['attributes']['name']}
+
+        {product['attributes'].get('description', 'Нет описания :(')}
+    """
+
+    keyboard = [
+        [InlineKeyboardButton('Назад', callback_data='back')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    context.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        photo=file_href,
+        caption=textwrap.dedent(fish_description),
+        reply_markup=reply_markup
+    )
+    context.bot.delete_message(
+        chat_id=update.effective_chat.id,
+        message_id=update.callback_query.message.message_id,
+    )
+    return 'HANDLE_MENU'
 
 
 def handle_users_reply(update: Update, context: CallbackContext):
@@ -60,7 +95,7 @@ def handle_users_reply(update: Update, context: CallbackContext):
 
     states_functions = {
         'START': start,
-        'ECHO': echo
+        'HANDLE_MENU': get_product_description
     }
     state_handler = states_functions[user_state]
 
