3bc20bebf11cd6f6a4dd212affd564b5e39e2372
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-07-31 19:40:47 +0500

    everything unwanted is removed

--- elasticpath_api_tools.py
+++ elasticpath_api_tools.py
@@ -1,7 +1,4 @@
-import os
-
 import requests
-from dotenv import load_dotenv
 
 
 def get_access_token(client_id, client_secret):
@@ -10,19 +7,6 @@ def get_access_token(client_id, client_secret):
         'client_id': client_id,
         'client_secret': client_secret,
         'grant_type': 'client_credentials'
-    }
-    response = requests.post(api_access_url, data=api_access_data)
-    response.raise_for_status()
-
-    access_token = response.json()['access_token']
-    return access_token
-
-
-def get_implicit_access_token(client_id):
-    api_access_url = 'https://useast.api.elasticpath.com/oauth/access_token'
-    api_access_data = {
-        'client_id': client_id,
-        'grant_type': 'implicit'
     }
     response = requests.post(api_access_url, data=api_access_data)
     response.raise_for_status()
@@ -53,23 +37,6 @@ def get_product(access_token, product_id):
 
     responce = requests.get(product_api_base_url, headers=headers)
     return responce.json()
-
-
-def create_cart(access_token, cart_id):
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
-    cart_api_url = 'https://useast.api.elasticpath.com/v2/carts'
-    cart_data = {
-        'data': {
-            'name': f'{cart_id}',
-            'id': cart_id
-        }
-    }
-
-    create_cart_responce = requests.post(cart_api_url, json=cart_data, headers=headers)
-    create_cart_responce.raise_for_status()
-    return create_cart_responce.json()
 
 
 def get_cart(access_token, cart_id):
@@ -156,25 +123,3 @@ def create_customer(access_token, cart_id, email):
     creating_customer_response = requests.post(cart_customers_url, headers=headers, json=customer_data)
     creating_customer_response.raise_for_status()
     return creating_customer_response.json()
-
-
-def get_customer(access_token, customer_id):
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
-
-    getting_customer_url = f'https://useast.api.elasticpath.com/v2/customers/{customer_id}'
-
-    response = requests.get(getting_customer_url, headers=headers)
-    response.raise_for_status()
-    return response.json()
-
-
-if __name__ == '__main__':
-    load_dotenv()
-
-    access_token = get_access_token(os.getenv('CLIENT_ID'), os.getenv('CLIENT_SECRET'))
-
-    losos_id = 'efe48323-9521-408a-9bfc-b673dc8a2725'
-
-    print(get_product(access_token, losos_id))
